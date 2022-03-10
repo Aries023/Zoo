5fff85cf521042916ae009eaca3684f635c34159
Aries023 <pet.potocka@gmail.com>
2022-03-10 23:39:03 +0100

    i just added the class Employee

---
+++ src/objects/Employee.java
@@ -0,0 +1,12 @@
+package objects;
+
+public class Employee {
+
+    String name;
+    byte age;
+
+    public Employee(String name, byte age) {
+        this.name = name;
+        this.age = age;
+    }
+}
