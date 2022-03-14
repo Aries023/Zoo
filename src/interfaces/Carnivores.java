8aab77232a3f2906e6b00b938bab409f55345e65
Aries023 <pet.potocka@gmail.com>
2022-03-14 22:34:43 +0100

    i just added interfaces Carnivores and Herbivoves in package interfaces

---
+++ src/interfaces/Carnivores.java
@@ -0,0 +1,10 @@
+package interfaces;
+
+public interface Carnivores {
+    boolean alevi = true;
+    boolean poisons = false;
+    byte dominance = 1;
+
+    boolean kill();
+    void died();
+}
---
+++ src/interfaces/Herbivoves.java
@@ -0,0 +1,9 @@
+package interfaces;
+
+public interface Herbivoves {
+    boolean alive = true;
+    boolean poisons = false;
+    byte dominance = 2;
+
+    void died();
+}
