957815224cac4f23f298640dcb37d37f3a0bde46
Aries023 <pet.potocka@gmail.com>
2022-03-16 20:36:09 +0100

    i just added description to interfaces Carnivores and Herbivoves

--- src/interfaces/Carnivores.java
+++ src/interfaces/Carnivores.java
@@ -1,5 +1,16 @@
 package interfaces;
 
+  /*
+  this is interface of all carnivores
+
+  - alive = represents if is animal alive or if it was eaten
+  - poisons = If the animal has been eaten, it can poison its enemy
+  - dominance = It is the ability to defend or attack.
+
+  - kill() = Attempted attack
+  - died() = The animal died and was eaten.
+   */
+
 public interface Carnivores {
     boolean alevi = true;
     boolean poisons = false;
--- src/interfaces/Herbivoves.java
+++ src/interfaces/Herbivoves.java
@@ -1,5 +1,15 @@
 package interfaces;
 
+ /*
+  this is interface of all herbivores
+
+  - alive = represents if is animal alive or if it was eaten
+  - poisons = If the animal has been eaten, it can poison its enemy
+  - dominance = It is the ability to defend or attack.
+
+  - died() = The animal died and was eaten.
+   */
+
 public interface Herbivoves {
     boolean alive = true;
     boolean poisons = false;
