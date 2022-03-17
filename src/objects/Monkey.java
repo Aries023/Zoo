bd005c3c456114d56c9e8843faa2771b3b27d600
Aries023 <pet.potocka@gmail.com>
2022-03-17 21:50:15 +0100

    i add a object Monkey

---
+++ src/objects/Monkey.java
@@ -0,0 +1,12 @@
+package objects;
+
+import abstracts.Herbivoves;
+
+public class Monkey extends Herbivoves {
+
+    byte dominance = 1;
+
+    protected void noise(){
+        System.out.println("hah uhh aah");
+    }
+}
