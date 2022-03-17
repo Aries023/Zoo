256dcc4574127417652b3a34e2e6738f6c07e369
Aries023 <pet.potocka@gmail.com>
2022-03-17 21:53:04 +0100

    i add a object Lion

---
+++ src/objects/Lion.java
@@ -0,0 +1,10 @@
+package objects;
+
+import abstracts.Carnivores;
+
+public class Lion extends Carnivores {
+
+    protected void noise(){
+        System.out.println("ROAAAAR Roar UAAAA!");
+    }
+}
