f3fff3abe30c4e75136ab6406395947955d4f9cb
Aries023 <pet.potocka@gmail.com>
2022-03-19 17:25:52 +0100

    i add a object PufferFish and create TextStream class for introduce and lead program

---
+++ src/objects/PufferFish.java
@@ -0,0 +1,8 @@
+package objects;
+
+import abstracts.Herbivoves;
+
+public class PufferFish extends Herbivoves {
+    boolean poisons =true;
+    byte dominance = 0;
+}
---
+++ src/stuffs/TextStream.java
@@ -0,0 +1,16 @@
+package stuffs;
+
+public class TextStream {
+    protected void intro(String name){
+        System.out.println("Hello there, welcome to our zoo!");
+        System.out.println(name + " has today shift. He will leads you.");
+        System.out.println(name + ": hello adventure, we have 5 cages.");
+        System.out.println(name + " now if you excuse me, i have to work.. enjoy your journey!");
+        System.out.println();
+    }
+
+    protected void whereToGo(){
+        System.out.println("where you want to go? put a number..");
+        System.out.println("0 = monkey / 1 = tyger / 2 = lion / 3 = parrot / 4 = pufferfish");
+    }
+}
