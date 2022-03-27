e970aa13813e44d969bd33b4d4af959465b5a3c7
Aries023 <pet.potocka@gmail.com>
2022-03-27 22:44:22 +0200

    i created a class for interact whit data.txt, class have only input method for now.

---
+++ src/Streams/ObjStream.java
@@ -0,0 +1,35 @@
+package Streams;
+
+import objects.Employee;
+
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+
+/**
+ * this is class for save and load data in txt document
+ */
+
+public class ObjStream {
+
+    /**
+     * This is metod for load data from txt doc.
+     * CARE !!! if someone delete data or somehow its happens that there is no data, there may come error! more in @return
+     * @return it will return employee from loaded data (if data are empty = basically return 0 length object array!!)
+     */
+
+    public static Employee [] loadData(){
+        Employee [] employeesData = new Employee[0];
+
+        try {
+
+            FileInputStream inputStream = new FileInputStream("data/data.txt");
+            ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
+            employeesData = (Employee[]) objectInputStream.readObject();
+            objectInputStream.close();
+        }catch (Exception e){
+            System.out.println("there is a error, probably you already didn't save any employee to data or something..");
+            e.getStackTrace();
+        }
+        return employeesData;
+    }
+}
